d310c62ec70245dd53b55b465dbc22b6fa2bccd0
SOmerandomdev-ui <[200~218329313+SOmerandomdev-ui@users.noreply.github.com~>
2025-08-03 22:45:38 -0400

    I made the button and made it prompt the user for grid numbers

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-let grid = document.querySelector(".Box")
+let button = document.querySelector(".change-grid")
+let grid = document.querySelector(".Container")
 
+//Makes a listener to if the button is clicked, depending on what the number is excuetes the make grid function 
+button.addEventListener("click", () => {
+let amount = prompt("What grid length do you want (Enter one number)");
 
-//Makes 1 row of 10 divs then adds more rows 
-for (i = 0; i < 10; i++) {
+row.remove();
+tiles.remove();
+
+for (i = 0; i < amount; i++) {
     let row = document.createElement("div")
     row.className = ("row")
     grid.appendChild(row)
 
-    for (j = 0; j < 10; j++) {
+    for (j = 0; j < amount; j++) {
         let tiles = document.createElement("div")
         tiles.className = "tiles"
         row.appendChild(tiles)
        
     }     
 }
+})
+
+
+
+
+
+
+//Makes 1 row of 10 divs then adds more rows 
